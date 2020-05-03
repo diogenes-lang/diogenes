8059e5801e001f3c9cd272fe5d70b3e43db832a9
Nicola Atzei <atzeinicola@gmail.com>
2020-05-03 12:24:15 +0200

    Fix mvn build

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,17 +2,19 @@
 
 FILEJAR=co2-honesty-checker-${CO2_HONESTY_CHECKER_VERSION}.jar
 
-echo "Downloading ${FILEJAR}"
+echo "Downloading ${FILEJAR}..."
 curl --fail -L --output ${FILEJAR} https://github.com/diogenes-lang/diogenes-honesty/releases/download/v${CO2_HONESTY_CHECKER_VERSION}/co2-honesty-checker-${CO2_HONESTY_CHECKER_VERSION}.jar
 
-[[ $? != 0 ]] && "There was an error downloading ${FILEJAR}, exiting..." && exit 1
+[[ $? != 0 ]] && echo "There was an error downloading ${FILEJAR}, exiting..." && exit 1
 
-echo "Installing "
+echo "Installing ${FILEJAR}..."
 mvn install:install-file \
->   -Dfile=${FILEJAR} \
->   -DgroupId=it.unica.co2 \
->   -DartifactId=co2-honesty-checker \
->   -Dversion=${CO2_HONESTY_CHECKER_VERSION} \
->   -Dpackaging=jar
+  -Dfile=${FILEJAR} \
+  -DgroupId=it.unica.co2 \
+  -DartifactId=co2-honesty-checker \
+  -Dversion=${CO2_HONESTY_CHECKER_VERSION} \
+  -Dpackaging=jar
 
-[[ $? != 0 ]] && "There was an error installing ${FILEJAR}, exiting..." && exit 2
+[[ $? != 0 ]] && echo "There was an error installing ${FILEJAR}, exiting..." && exit 2
+
+exit 0
